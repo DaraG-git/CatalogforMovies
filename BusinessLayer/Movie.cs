e759b8560a0d16580adf2cb14fd3c6a0c06870c6
Dara Georgieva <georgievatdara@gmail.com>
2025-05-02 18:36:29 +0300

    Add project files.

---
+++ BusinessLayer/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection.Emit;
+
+namespace BusinessLayer
+{
+    public abstract class Movie
+    {
+        public int MovieID { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public Genre Genre { get; set; }
+        public int ReleaseYear { get; set; }
+        public decimal Review { get; set; }
+    }
+}
---
+++ BusinessLayer/Ranking.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class Ranking
+    {
+        public int ID { get; set; }
+        public Viewer Viewers { get; set; }
+        public List<Movie> Movies { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
